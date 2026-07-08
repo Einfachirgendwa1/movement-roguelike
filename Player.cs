437407d02b436c273eb9d181ea46381b2ef90718
Einfachirgendwa1 <153438063+Einfachirgendwa1@users.noreply.github.com>
2026-07-08 17:51:04 +0200

    added gravity

--- Player.cs
+++ Player.cs
@@ -3,5 +3,9 @@ using Godot;
 namespace MovementRoguelike3D;
 
 public partial class Player : CharacterBody3D {
+    public override void _PhysicsProcess(double delta) {
+        Velocity += GetGravity();
 
+        MoveAndSlide();
+    }
 }
