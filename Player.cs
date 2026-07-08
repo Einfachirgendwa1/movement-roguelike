5f5a5b29f55342dba8621b1c1168630342113cd5
Einfachirgendwa1 <153438063+Einfachirgendwa1@users.noreply.github.com>
2026-07-08 18:14:49 +0200

    wudnerbar

--- Player.cs
+++ Player.cs
@@ -3,9 +3,36 @@ using Godot;
 namespace MovementRoguelike3D;
 
 public partial class Player : CharacterBody3D {
+    public override void _Ready() {
+        Input.SetMouseMode(Input.MouseModeEnum.Captured);
+    }
+
     public override void _PhysicsProcess(double delta) {
         Velocity += GetGravity();
 
+        int left = Input.IsActionPressed("Left") ? 1 : 0;
+        int right = Input.IsActionPressed("Right") ? 1 : 0;
+        int xAxis = right - left;
+
+        int forward = Input.IsActionPressed("Forward") ? 1 : 0;
+        int backward = Input.IsActionPressed("Backward") ? 1 : 0;
+        int zAxis = backward - forward;
+
+        Vector3 direction = new(xAxis, 0, zAxis);
+        Vector3 movement = direction.Normalized() * Settings.MovementSpeed;
+        movement.Y = Velocity.Y;
+        Velocity = movement;
+
         MoveAndSlide();
+    }
+
+    public override void _Process(double delta) {
+        if (Input.IsActionJustPressed("Escape")) {
+            Input.SetMouseMode(Input.MouseModeEnum.Visible);
+        }
+    }
+
+    public override void _Input(InputEvent @event) {
+        if (@event is InputEventMouseMotion mouseMotion) { }
     }
 }
---
+++ Settings.cs
@@ -0,0 +1,5 @@
+﻿namespace MovementRoguelike3D;
+
+public class Settings {
+    public static float MovementSpeed => 5f;
+}
