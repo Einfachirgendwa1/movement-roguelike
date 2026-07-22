80728669b071260c458b333c636f623c13bde8e3
JoJo <j.mueller.richmann@icloud.com>
2026-07-22 11:15:24 +0200

    fall of if u are over max speed

--- GameState.cs
+++ GameState.cs
@@ -37,7 +37,7 @@ public partial class GameState : Node {
     [Export] public float GroundDrag = 0.80f;
     [Export] public float AirDrag = 0.95f;
 
-    public static float SprintDrag(float x, float drag) =>
+    public static float OverMaxSpeedDrag(float x, float drag) =>
         Mathf.Lerp(1, drag, Mathf.Clamp((x - 2) * 0.05f, 0f, 1f));
 
     #endregion
